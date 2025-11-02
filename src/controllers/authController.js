71ab5098ee188004535c198a657cd69c058c4a51
rahul-kapgate <rahulkapgateyt@gmail.com>
2025-11-02 23:00:59 +0530

    typo fix for id in login api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -58,7 +58,7 @@ const login = async (req, res, next) => {
     // 6️⃣ Send response
     res.status(200).json({
       message: "Login successful",
-      user: { id: user._id, email: user.email, mobile: user.mobile, is_admin: user.is_admin, },
+      user: { id: user.id, email: user.email, mobile: user.mobile, is_admin: user.is_admin, },
       accessToken,
       refreshToken,
     });
