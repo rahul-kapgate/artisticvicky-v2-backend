c5da30fcb44120e29365eafd1c17a6399d447297
rahul-kapgate <rahulkapgateyt@gmail.com>
2026-03-14 23:42:16 +0530

    upadted the api for master class

--- src/routes/masterclassRoutes.js
+++ src/routes/masterclassRoutes.js
@@ -2,14 +2,14 @@ import express from "express";
 import {
   createMasterclass,
   updateMasterclass,
-  uploadMasterclassFiles,
+  uploadMasterclassImage,
 } from "../controllers/masterclassController.js";
 import { verifyToken } from "../middlewares/authMiddleware.js";
 import { isAdmin } from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", verifyToken, isAdmin, uploadMasterclassFiles, createMasterclass);
-router.put("/:id", verifyToken, isAdmin, uploadMasterclassFiles, updateMasterclass);
+router.post("/", verifyToken, isAdmin, uploadMasterclassImage, createMasterclass);
+router.put("/:id", verifyToken, isAdmin, uploadMasterclassImage, updateMasterclass);
 
 export default router;
