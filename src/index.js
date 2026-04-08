b1cf35dae4fdb33ce7dfa8d36a82485a738efe9a
rahul-kapgate <rahulkapgateyt@gmail.com>
2026-04-08 17:44:14 +0530

    ssl issue fixed

--- src/index.js
+++ src/index.js
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
 
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running is on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
