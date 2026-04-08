1c2bfe213e5f20d8248017af053cbb62d0811784
rahul-kapgate <rahulkapgateyt@gmail.com>
2026-04-08 17:45:10 +0530

    port issue fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import app from "./app.js";
 import { supabase } from "./config/supabaseClient.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to DB 
 supabase;
