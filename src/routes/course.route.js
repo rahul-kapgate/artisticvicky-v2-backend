9d374f20bfae963f31d7fef9285e2e2e1f0d5b2f
rahul-kapgate <rahulkapgateyt@gmail.com>
2026-09-19 19:02:29 +0530

    feat(courses): add public course APIs and schemas for course details and listing

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -3,6 +3,6 @@ import { Router } from "express";
 import courseRoute from "../modules/course/public/course.routes.js";
 const router = Router();
 
-router.use("/courses", courseRoute);
+router.use("/", courseRoute);
 
 export default router;
